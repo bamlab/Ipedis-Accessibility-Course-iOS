b6e658c8a15afd738130622226862c1bd6d9b2ff
Dennis BORDET <dennisb@bam.tech>
2024-11-13 10:39:29 +0100

    Initial Commit

---
+++ Ipedis Accessibility Course/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ipedis Accessibility Course
+//
+//  Created by Dennis BORDET on 13/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ipedis Accessibility Course/Ipedis_Accessibility_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ipedis_Accessibility_CourseApp.swift
+//  Ipedis Accessibility Course
+//
+//  Created by Dennis BORDET on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Ipedis_Accessibility_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
